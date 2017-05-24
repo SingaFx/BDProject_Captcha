5adf423c5843c369133213bc569d0963bf147599
Mikey <mikeyjou@gmail.com>
2017-05-25 01:08:28 +0800

    Initial commit

---
+++ captcha.py
@@ -0,0 +1,66 @@
+from PIL import Image, ImageTk, ImageEnhance
+import numpy as np
+import cv2
+
+folder = 'nccu/'
+
+#處理驗證碼
+def Captcha():
+    for t in range(0,100):
+        img = Image.open('img/' + folder + str(t) + '.bmp', 'r')
+        print('讀取' + 'img/' + folder + str(t) + '.bmp...')
+        # enhancer = ImageEnhance.Contrast(img)
+        # img = enhancer.enhance(5.0)
+        # enhancer = ImageEnhance.Brightness(img)
+        # img = enhancer.enhance(10.0)
+        pixdata = img.load()
+        img.save('img/' + folder + str(t) + '_bright.bmp')
+        # for y in range(img.size[1]): 
+            # for x in range(img.size[0]): 
+                # if pixdata[x, y][0] < 10: 
+                    # pixdata[x, y] = (0, 0, 0, 255) 
+        # for y in range(img.size[1]): 
+            # for x in range(img.size[0]): 
+                # if pixdata[x, y][1] < 50: 
+                    # pixdata[x, y] = (0, 0, 0, 255) 
+        # for y in range(img.size[1]): 
+            # for x in range(img.size[0]): 
+                # if pixdata[x, y][2] > 0:
+                    # pixdata[x, y] = (255, 255, 255, 255)
+                    
+        # for x in range(img.width):
+        #     for y in range(img.height):
+        #         if pixdata[x , y][1] != 255 and pixdata[x , y][2] == 255:
+        #             pixdata[x , y] = (0 , 0 , 0 , 255)
+        #         else:
+        #             pixdata[x , y] = (255 , 255 , 255 , 255)
+
+        count = 0
+        for i in range(img.width):
+            for j in range(img.height):
+                # if pixdata[i,j][0] == 0 and pixdata[i,j][1] == 0 and pixdata[i,j][2] == 0:
+                count = 0 
+                for k in range(1, -2, -1):
+                    for l in range(1, -2, -1):
+                        try:
+                            if i + k >= 0 and i + k < img.width and j + l >= 0 and j + l < img.height:
+                                if pixdata[i+k,j+l][0] == 255 and pixdata[i+k,j+l][1] == 255 and pixdata[i+k,j+l][2] == 255:
+                                    count += 1
+                        except TypeError:
+                            pass
+                if count >= 7:
+                    pixdata[i,j] = (255 , 255 , 255 , 255)    
+         
+        img.save('img/' + folder + str(t) + '_black.bmp')
+
+        im = cv2.imread('img/' + folder + str(t) + '_black.bmp' , 0)
+
+        kernel = np.ones((2 , 2) , np.uint8)
+        opening = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel)
+
+        #opening = cv2.blur(opening , (2, 2))
+        #opening = cv2.dilate(opening, (1, 1), iterations=1)
+        cv2.imwrite('img/' + folder + str(t) + '_black2.bmp', opening)
+
+if __name__ == "__main__":
+    Captcha()
